84b54b994332912e52ccf42a6ab2be613c52d55d
Aliendl3bar <aymanesalmoune00@gmail.com>
2025-12-29 13:09:41 +0100

    fix cat

--- prototype.sql
+++ prototype.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE base_relas2;
-use base_relas2;
+CREATE DATABASE blog_db;
+use blog_db;
 
 CREATE TABLE user(
 id INT auto_increment primary key,
@@ -7,15 +7,21 @@ name varchar(100) not null,
 email varchar(100) unique,
 password varchar(225) not null
 );
+create table category(
+id int auto_increment primary key,
+category_name varchar(100) not null
+);
 
 CREATE TABLE article(
 id INT auto_increment primary key,
 tittle varchar(225) not null,
 content text not null,
 status_public TINYINT(1) default 0,
-datep date,
+date_publication date,
 id_user INT,
-foreign key (id_user) references user(id)
+id_category INT,
+foreign key (id_user) references user(id),
+foreign key (id_category) references category(id)	
 );
 
 CREATE TABLE comment(
@@ -24,14 +30,7 @@ content text not null,
 datep date,
 id_user INT,
 id_article INT,
-id_category INT,
 foreign key (id_user) references user(id),
-foreign key (id_article) references article(id),
-foreign key (id_category) references category(id)
-
+foreign key (id_article) references article(id)
 );
 
-create table category (
-id int auto_increment primary key,
-tittle varchar(100) not null
-);
